f938aa844dc4465c7e5d757d3e16c58da2f31aef
Shruti Rajput <shrutii.r765@gmail.com>
2026-03-01 14:38:13 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,26 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
+import messageRoutes from './routes/message.route.js';
+import path from 'path';
 
 const app = express();
-app.use(express.json());
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
+
+//make ready for deployment
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(_,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    });
+}
     
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
-})
+});
