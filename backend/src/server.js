0dc23d6711b3aaef854a609f67618c59a25b8db7
Shruti Rajput <shrutii.r765@gmail.com>
2026-03-06 16:36:21 +0530

    Welcome email changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,20 +3,20 @@ import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js';
 import path from 'path';
 import { connectDB } from './lib/db.js';
+import { ENV } from './lib/env.js';
 
 const app = express();
-import dotenv from "dotenv";
-dotenv.config();
+
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 app.use(express.json()); //req.body will be undefined without this middleware
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
 
 //make ready for deployment
-if(process.env.NODE_ENV === "production"){
+if(ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
     app.get("*",(_,res)=>{
