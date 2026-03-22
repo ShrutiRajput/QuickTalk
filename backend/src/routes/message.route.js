94780abd5416019e68e3d7982e067a5e3259d4fe
Shruti Rajput <shrutii.r765@gmail.com>
2026-03-22 21:08:39 +0530

    sidebar fe changes done

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.use(arcjetProtection, protectRoute);
 
-router.get("/contact", getAllContacts);
+router.get("/contacts", getAllContacts);
 router.get("/chats", getChatPartners);
 router.get("/:id", getMessagesByUserId);
 router.post("/send/:id", sendMessage);
